b06838e82de3522259b3977128b4f16185cf2d4a
Komal Meena <80938016+komalmeena6516@users.noreply.github.com>
2023-04-13 15:08:21 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,12 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+
+import React from "react";
+
+const App = () =>{
+return(
+    <div>
+        <h1>Hello world</h1>
+    </div>
+)
+}
+export default App;
